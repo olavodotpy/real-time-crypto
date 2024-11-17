4625b8117ff9a326ca88220a1459c51a6875b875
olavodotpy <olavoazevedo88@gmail.com>
2024-11-17 04:02:20 -0300

    deployment done successfully

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^bc)9=^2fj3_m$6o^65+w5k#rsw8n4ea7=mn#@f7=lz=&gfyy3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['real-time-crypto-production.up.railway.app', 'www.real-time-crypto-production.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'real-time-crypto-production.up.railway.app', 'www.real-time-crypto-production.up.railway.app']
 
 
 # Application definition
