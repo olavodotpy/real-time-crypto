2dbc426b088bda740a967c0d99095363452dc162
olavodotpy <olavoazevedo88@gmail.com>
2024-11-17 03:48:57 -0300

    deployment done successfully

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^bc)9=^2fj3_m$6o^65+w5k#rsw8n4ea7=mn#@f7=lz=&gfyy3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['real-time-crypto-production.up.railway.app/', '127.0.0.1']
 
