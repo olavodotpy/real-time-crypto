74f3c6e524cda0a72d8e495043ed71f32bbe275d
olavodotpy <olavoazevedo88@gmail.com>
2024-11-18 00:00:53 -0300

    Test REST #14

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 
 
 ALLOWED_HOSTS = [
+    'real-time-crypto-production.up.railway.appreal-time-crypto-production.up.railway.app',
     'http://127.0.0.1:8000',
     'http://localhost:3000',
 ]
