f09ad1d03c05d0a9af345a9b9c736b51518c8849
olavodotpy <olavoazevedo88@gmail.com>
2024-11-17 23:14:52 -0300

    Test REST #8

--- django_project/settings.py
+++ django_project/settings.py
@@ -127,10 +127,10 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    "/home/static/home",
+    "/home/static",
 ]
 
-STATIC_ROOT = "home/static/home/"
+STATIC_ROOT = "home/"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
